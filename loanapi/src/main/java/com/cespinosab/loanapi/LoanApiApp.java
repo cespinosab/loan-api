b2ab99525d07e24096feb6daf4209518d2c78300
Cristina Espinosa Borge <cristinaespinosaborge@gmail.com>
2026-02-04 21:02:14 +0100

    Added structure including poms, .gitignore and basic skeleton

---
+++ loanapi/src/main/java/com/cespinosab/loanapi/LoanApiApp.java
@@ -0,0 +1,13 @@
+package com.cespinosab.loanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApiApp.class, args);
+    }
+
+}
---
+++ loanapi/src/test/java/com/cespinosab/loanapi/LoanApiAppTest.java
@@ -0,0 +1,13 @@
+package com.cespinosab.loanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
