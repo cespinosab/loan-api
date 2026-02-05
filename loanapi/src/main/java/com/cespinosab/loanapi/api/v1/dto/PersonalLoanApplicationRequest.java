9f0a57df7c0b9156368d32f9154109ae6c81afb0
Cristina Espinosa Borge <cristinaespinosaborge@gmail.com>
2026-02-05 20:03:38 +0100

    Added missed get/set

--- loanapi/src/main/java/com/cespinosab/loanapi/api/v1/dto/PersonalLoanApplicationRequest.java
+++ loanapi/src/main/java/com/cespinosab/loanapi/api/v1/dto/PersonalLoanApplicationRequest.java
@@ -28,4 +28,68 @@ public class PersonalLoanApplicationRequest implements Serializable {
     private OffsetDateTime createdAt;
 
     private OffsetDateTime modifiedAt;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPersonalId() {
+        return personalId;
+    }
+
+    public void setPersonalId(String personalId) {
+        this.personalId = personalId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    public void setBadge(String badge) {
+        this.badge = badge;
+    }
+
+    public PersonalLoanApplicationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PersonalLoanApplicationStatus status) {
+        this.status = status;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(OffsetDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
 }
--- loanapi/src/main/java/com/cespinosab/loanapi/api/v1/dto/PersonalLoanApplicationResponse.java
+++ loanapi/src/main/java/com/cespinosab/loanapi/api/v1/dto/PersonalLoanApplicationResponse.java
@@ -1,10 +1,105 @@
 package com.cespinosab.loanapi.api.v1.dto;
 
+import com.cespinosab.loanapi.model.enums.PersonalLoanApplicationStatus;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import java.io.Serializable;
+import java.time.OffsetDateTime;
 
 /**
  * Represent the response of a /v1/loadApplications endpoint
  */
 public class PersonalLoanApplicationResponse implements Serializable {
 
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String personalId;
+
+    private double amount;
+
+    private String badge;
+
+    @JsonDeserialize(using = JsonDeserializer.class)
+    private PersonalLoanApplicationStatus status;
+
+    private OffsetDateTime createdAt;
+
+    private OffsetDateTime modifiedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPersonalId() {
+        return personalId;
+    }
+
+    public void setPersonalId(String personalId) {
+        this.personalId = personalId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    public void setBadge(String badge) {
+        this.badge = badge;
+    }
+
+    public PersonalLoanApplicationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PersonalLoanApplicationStatus status) {
+        this.status = status;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(OffsetDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
 }
